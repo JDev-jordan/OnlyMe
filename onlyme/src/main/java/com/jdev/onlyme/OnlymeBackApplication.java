9b7e052c1db839103b5c43f12148a69b6d265581
JDev-jordan <75861498+JDev-jordan@users.noreply.github.com>
2022-04-19 11:36:15 +0200

    creation projet

---
+++ onlyme/src/main/java/com/jdev/onlyme/OnlymeBackApplication.java
@@ -0,0 +1,13 @@
+package com.jdev.onlyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlymeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlymeBackApplication.class, args);
+	}
+
+}
---
+++ onlyme/src/test/java/com/jdev/onlyme/OnlymeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdev.onlyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlymeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
